583390608f1f1c7751a07bb68057e75aad52e360
longvibol <vibollong@gmail.com>
2024-11-27 16:54:47 +0700

    wer

--- 2 Advance Basic/adv_basics/lib/main.dart
+++ 2 Advance Basic/adv_basics/lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(HomeView());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.deepPurple,
+              Color.fromARGB(255, 167, 50, 173),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
 
+/*
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
@@ -60,3 +76,4 @@ class HomeView extends StatelessWidget {
     );
   }
 }
+*/
